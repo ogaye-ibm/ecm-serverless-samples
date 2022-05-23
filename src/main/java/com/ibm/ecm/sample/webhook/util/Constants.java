6786d00764045a15120332803c53d75abccb3876
Omar Gaye <omar.gaye@ibm.com>
2022-05-23 08:22:31 -0400

    New HMAC

--- src/main/java/com/ibm/ecm/sample/webhook/util/Constants.java
+++ src/main/java/com/ibm/ecm/sample/webhook/util/Constants.java
@@ -18,7 +18,7 @@ public class Constants {
 
     // Webhook Event Action properties
     public static final String WEBHOOK_RECEIVER_REGISTRATION_ID = "webhook";
-    public static final String HMAC_CREDENTIAL_SECRET = "621B5DBA-107D-4B0D-85C5-71AF8DF59E70";
+    public static final String HMAC_CREDENTIAL_SECRET = "0e58ac81-2851-423c-9ee5-91d6461a7ff2";
     
     /*
      * ServletContext attribute key constants, which are used to share data
